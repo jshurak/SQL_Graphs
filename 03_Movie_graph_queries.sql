f52ac561879e52d878c498c2c1ced60e1a06a8e7
Jeffrey Shurak <jshurak@anexinet.com>
2019-06-19 12:13:22 -0400

    renaming

--- 03_Movie_graph_queries.sql
+++ 03_Movie_graph_queries.sql
@@ -13,20 +13,17 @@ from Movies,Actor, StarredIn
 where match(actor-(starredIn)->Movies)
 and FirstName = 'Chris' and lastName = 'Hemsworth'
 
---get Chris Hemsworth co-stars
-with hemsworth_movies
-as
-(
-	select movieID,actorId
-	from Movies,Actor, StarredIn
-	where match(actor-(starredIn)->Movies)
-	and FirstName = 'Chris' and lastName = 'Hemsworth'
+
+--Get Chris Hemsworth co stars
+select Title,a2.FirstName,a2.LastName
+from Movies,Actor a,StarredIn, actor a2, starredIn s2
+where match(
+	a-(starredIn)->Movies
+	and
+	Movies-(s2)->a2
 )
-select Title,FirstName,LastName
-from Movies m,Actor a,StarredIn s
-where match(m-(s)->a)
-and movieID in (select movieId from hemsworth_movies)
-and ActorID not in (select actorId from hemsworth_movies)
+and a.ActorID = (select actorID from actor where FirstName = 'Chris' and lastName = 'Hemsworth')
+and a2.actorId <> (select actorID from actor where FirstName = 'Chris' and lastName = 'Hemsworth')
 
 --get actors that have NOT co starred with Chris Hemsworth
 with hemsworth_movies
